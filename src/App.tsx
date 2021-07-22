0600a23c093b2b4f0859ae8c95c71c6909538cba
alwin <winalrollevi@gmail.com>
2021-07-22 14:39:01 +0800

    more shitty code, at this point idk what I'm even doing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/style.css'
 import { TodoContainer } from './components/TodoContainer';
 
+const initialTodos: Array<Todo> = [];
+
 const App: React.FC = () => {
+
+  const [todos, setTodos] = useState(initialTodos);
+
+  // const toggleTodo: ToggleTodo = selectedTodo => {
+  //   const newTodos = todos.map(todo => {
+  //     if (todo === selectedTodo)
+  //   })
+  // }
+
+
+
   return (
     <div className="App">
       <TodoContainer />
     </div>
   );
+
+
 }
 
 export default App;
