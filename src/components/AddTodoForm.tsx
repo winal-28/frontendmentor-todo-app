4f72e3f47bfbca263ea09f1f46d69974a78c93ad
alwin <winalrollevi@gmail.com>
2021-07-22 09:46:29 +0800

    polished up the add todo form a bit, organized stylesheets

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -1,7 +1,8 @@
 export const AddTodoForm = () => {
     return (
         <form className='addTodo'>
-            <input type="checkbox" className="submit"/>
+            <input type="checkbox" className="checkbox submit"/>
+              <span className="checkmark"></span>
             <input type="text" placeholder="Create a new todo..." className="todoInput"/>
         </form>
     )
